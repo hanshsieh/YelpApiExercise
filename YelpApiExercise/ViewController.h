96d2cd85a82d63b929c43ca458701df7b3bb41e7
Chu-An Hsieh <cahsieh@yahoo-inc.com>
2015-06-23 10:50:23 +0800

    Initial commit.

---
+++ YelpApiExercise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YelpApiExercise
+//
+//  Created by Chu-An Hsieh on 6/23/15.
+//  Copyright (c) 2015 Chu-An Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YelpApiExercise/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YelpApiExercise
+//
+//  Created by Chu-An Hsieh on 6/23/15.
+//  Copyright (c) 2015 Chu-An Hsieh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
